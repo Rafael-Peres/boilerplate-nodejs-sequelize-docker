ca2a2b094087f0445e331b633122f9a947a61f43
Rafael Peres <rafaelperes199@gmail.com>
2020-03-31 23:24:09 -0300

    adjusted name table

--- src/database/migrations/20200328043523-create-users.js
+++ src/database/migrations/20200328043523-create-users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-   return queryInterface.createTable('users_ovo', {
+   return queryInterface.createTable('users', {
     id: {
       type: Sequelize.INTEGER,
       allowNull: false,
